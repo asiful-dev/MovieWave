6ef988ddcbaf20e318b010361edbd2ed92a1cf9a
AwfulAsiful <awful872@gmail.com>
2025-07-03 21:47:56 +0600

    fix the import extension issue

--- server/src/routes/show.routes.js
+++ server/src/routes/show.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { getNowPlayingMovies, getUpcomingMovies } from "../controllers/show.controller";
+import { getNowPlayingMovies, getUpcomingMovies } from "../controllers/show.controller.js";
 
 
 const router = Router();
