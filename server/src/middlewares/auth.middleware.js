38828e9c40bb4cdbf83eda0685e4809c523f87b4
AwfulAsiful <awful872@gmail.com>
2025-07-11 21:07:07 +0600

    nothing substantial just removed few extra spaces

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,17 @@
+import { clerkClient } from "@clerk/express";
+import AsyncHandler from "../utils/AsyncHandler.js";
+import ApiError from "../utils/ApiError.js";
+
+const protectAdmin = AsyncHandler(async (req, _, next) => {
+    const { userId } = req.auth();
+    const user = await clerkClient.users.getUser(userId);
+
+    if (!user) throw new ApiError(404, "User Not Found");
+    
+    if (user.privateMetadata.role !== "admin")
+        throw new ApiError(401, "Not Authorized")
+
+    next();
+})
+
+export default protectAdmin;
