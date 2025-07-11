d3cec2304fce1cc017f96e15da3e20fdc7dee731
AwfulAsiful <awful872@gmail.com>
2025-07-11 18:08:31 +0600

    Add Inngest client and user event functions for creation, deletion, and updates

---
+++ server/src/Inngest/inngest.js
@@ -0,0 +1,86 @@
+import { Inngest } from "inngest"
+import User from "../models/user.models.js";
+import ApiError from "../utils/ApiError.js"
+
+//new inngest client
+const inngest = new Inngest({
+    id: "MovieWave"
+});
+
+
+const userCreation = inngest.createFunction(
+    { id: "user-creation" },
+    { event: "clerk/user.created" },
+    async ({ event }) => {
+        try {
+            const { id, first_name, last_name, email_addresses, image_url } = event.data;
+
+            const user = {
+                _id: id,
+                name: first_name + " " + last_name,
+                email: email_addresses[0].email_address,
+                avatar: image_url
+            }
+
+            await User.create(user);
+        } catch (error) {
+            console.log("Failed to create user ", error);
+
+        }
+    }
+)
+
+
+const userDeleted = inngest.createFunction(
+    { id: "user-deleted" },
+    { event: "clerk/user.deleted" },
+    async ({ event }) => {
+        try {
+            const { id } = event.data;
+            const response = await User.findOneAndDelete(id);
+            if (!response) throw new ApiError(404, "User Not Found!")
+        } catch (error) {
+            console.log("Failed to delete user", error);
+
+        }
+
+    }
+)
+
+const userUpdated = inngest.createFunction(
+    { id: "user-updated" },
+    { event: "clerk/user.updated" },
+    async ({ event }) => {
+        try {
+            const { id, first_name, last_name, email_addresses, image_url } = event.data;
+            const updatedUser = {
+                _id: id,
+                name: first_name + " " + last_name,
+                email: email_addresses[0].email_address,
+                avatar: image_url
+            }
+            const response = await User.findByIdAndUpdate(id, updatedUser);;
+            if (!response) throw new ApiError(404, "User Not Found!")
+        } catch (error) {
+            console.log("Failed to update user ", error);
+        }
+    }
+)
+
+
+
+
+
+
+
+
+const functions = [
+    userCreation,
+    userDeleted,
+    userUpdated
+]
+
+export {
+    inngest,
+    functions
+}
