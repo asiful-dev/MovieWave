94713537dadf2f86922305a690fe4fc7921e58f0
AwfulAsiful <awful872@gmail.com>
2025-07-11 17:39:08 +0600

    Added database models/schemas

---
+++ server/src/models/booking.models.js
@@ -0,0 +1,31 @@
+import { Schema, model } from "mongoose";
+
+const bookingSchema = new Schema({
+    user: {
+        type: String,
+        required: true,
+        ref: 'User'
+    },
+    show: {
+        type: String,
+        required: true,
+        ref: "Show"
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    bookedSeats: {
+        type: Array,
+        required: true
+    },
+    isPaid: {
+        type: Boolean,
+        default: false
+    },
+    paymentLink: {
+        type: String
+    }
+}, { timestamps: true })
+
+export const Booking = model("Booking", bookingSchema);
---
+++ server/src/models/user.models.js
@@ -0,0 +1,23 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+    _id: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        required: true
+
+    }
+}, { timestamps: true })
+
+export const User = model("User", userSchema);
