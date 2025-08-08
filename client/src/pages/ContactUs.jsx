1e7e541bf726a79a14efcb768e05c3f4fd408684
shehabrakib <shehabprogram128@gmail.com>
2025-08-08 21:27:35 +0600

    changed the animation for the contact form

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -87,7 +87,7 @@ const ContactUs = () => {
         </div>
 
         {/* Contact Form */}
-        <div className="mt-16 animate-fade-in">
+        <div className="mt-16 animate-fade-out">
           <div className="max-w-4xl mx-auto">
             <h2 className="text-3xl md:text-4xl font-bold text-white mb-8 text-center">
               Send us a Message
