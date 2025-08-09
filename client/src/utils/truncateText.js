e37fa523002dae2d46fd64a294fa1120a9e71138
shaharearemon1 <shaharearemon1@gmail.com>
2025-08-09 07:01:27 +0600

    increased the length of the truncated text

--- client/src/utils/truncateText.js
+++ client/src/utils/truncateText.js
@@ -1,4 +1,4 @@
-const truncateText = (text, maxLength = 200) => {
+const truncateText = (text, maxLength = 220) => {
   if (!text) return "";
   return text.length > maxLength
     ? text.slice(0, text.lastIndexOf(" ", maxLength)) + "..."
